58128b35a952204aa1f417a512e6488dc27b2101
Andres <andrex200316@gmail.com>
2020-12-04 21:28:19 -0500

    Changes in url

--- core/urls.py
+++ core/urls.py
@@ -6,8 +6,8 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.contrib import admin
 from django.urls import path, include  # add this
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
